3caa4941a513a08ce6ef94a5e483cafabd3135e0
Rickey Johnson <mr.rixter@gmail.com>
2023-01-05 01:03:11 +0000

    renamed func component

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 
-import { getPosts, getPostDetails} from '../../services'
-import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components'
+import { getPostDetails } from '../../services'
+import { PostDetail as PostDetailComponent, Author, Comments, CommentsForm, Loader } from '../../components'
 
 const PostDetail = ({ post }) => {
   const router = useRouter();
@@ -10,23 +10,13 @@ const PostDetail = ({ post }) => {
   if(router.isFallback) {
     return <Loader />
   }
-  
+
   return (
     <div className='container mx-auto px-10 mb-8'>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='col-span-1 lg:col-span-8'>
-          <PostDetail post={post} />
-          <Author author={post.author} />
-          <CommentsForm slug={post.slug} />
-          <Comments slug={post.slug} />
-        </div>
-        <div className='col-span-1 lg:col-span-4'>
-          <div className='relative lg:sticky top-8'>
-            <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
-            <Categories />
-          </div>
-        </div>
-      </div>
+      <PostDetailComponent post={post} />
+      <Author author={post.author} />
+      <CommentsForm slug={post.slug} />
+      <Comments slug={post.slug} />
     </div>
   )
 }
@@ -45,6 +35,7 @@ export async function getStaticPaths() {
   const posts = await getPosts();
 
   return {
-    paths: posts.map(({ node: { slug }}) => ({ params: { slug }})), fallback: true,
+    paths: posts.map(({ node: { slug }}) => ({ params: { slug }})),
+    fallback: true,
   }
 }
