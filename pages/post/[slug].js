897d9e1a86850a98535c95d9ff4c124dc7edd9a1
Rickey Johnson <mr.rixter@gmail.com>
2023-01-05 01:07:01 +0000

    renamed func component

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,41 +1,47 @@
-import React from 'react'
-import { useRouter } from 'next/router'
+import React from "react";
+import { useRouter } from "next/router";
 
-import { getPostDetails } from '../../services'
-import { PostDetail as PostDetailComponent, Author, Comments, CommentsForm, Loader } from '../../components'
+import { getPostDetails } from "../../services";
+import {
+  PostDetail as PostDetailComponent,
+  Author,
+  Comments,
+  CommentsForm,
+  Loader,
+} from "../../components";
 
-const PostDetail = ({ post }) => {
+const PostDetailPage = ({ post }) => {
   const router = useRouter();
 
-  if(router.isFallback) {
-    return <Loader />
+  if (router.isFallback) {
+    return <Loader />;
   }
 
   return (
-    <div className='container mx-auto px-10 mb-8'>
+    <div className="container mx-auto px-10 mb-8">
       <PostDetailComponent post={post} />
       <Author author={post.author} />
       <CommentsForm slug={post.slug} />
       <Comments slug={post.slug} />
     </div>
-  )
-}
+  );
+};
 
-export default PostDetail
+export default PostDetailPage;
 
 export async function getStaticProps({ params }) {
-  const data = await getPostDetails(params.slug)
+  const data = await getPostDetails(params.slug);
 
   return {
-    props: { post: data }
-  }
+    props: { post: data },
+  };
 }
 
 export async function getStaticPaths() {
   const posts = await getPosts();
 
   return {
-    paths: posts.map(({ node: { slug }}) => ({ params: { slug }})),
+    paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
     fallback: true,
-  }
+  };
 }
